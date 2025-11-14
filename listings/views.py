22fdb7036973b9a520dc8ec417165c1ab134b97a
EllisBale <elscar29@gmail.com>
2025-11-14 11:59:37 +0000

    Add view function for property detail page

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Property
 
 
@@ -9,3 +9,8 @@ def property_list(request):
 
     properties = Property.objects.all()
     return render(request, "listings/property_list.html", {"properties": properties})
+
+
+def property_detail(request, pk):
+    property_obj = get_object_or_404(Property, pk=pk)
+    return render(request, "listings/property_detail.html", {"property": property_obj})
