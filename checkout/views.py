0e6554f21262b938f41ff81878b1cace62bbb24a
EllisBale <elscar29@gmail.com>
2025-11-25 15:58:39 +0000

    Add webhooks.py and webhook_handler.py to checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .forms import OrderForm
+from bookings.models import Booking
 
-# Create your views here.
+
+def checkout(request, booking_id):
+    booking = get_object_or_404(Booking, pk=booking_id)
---
+++ checkout/webhook_handler.py
@@ -0,0 +1,51 @@
+from django.http import HttpResponse
+from bookings.models import Booking
+
+
+class StripeWH_Handler:
+    """Handle Stripe webhooks"""
+
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        """Fallback for any event not specifically handled"""
+        return HttpResponse(
+            content=f"Unhandled event type: {event['type']}",
+            status=200
+        )
+
+    def handle_payment_intent_succeeded(self, event):
+        """Handle successful payment"""
+        intent = event.data.object
+
+        # Read the booking ID from metadata
+        booking_id = intent.metadata.get("booking_id")
+
+        if not booking_id:
+            return HttpResponse(
+                content="No booking ID in metadata",
+                status=400,
+            )
+
+        try:
+            booking = Booking.objects.get(id=booking_id)
+            booking.is_paid = True
+            booking.save()
+        except Booking.DoesNotExist:
+            return HttpResponse(
+                content="Booking not found",
+                status=404
+            )
+
+        return HttpResponse(
+            content="Payment confirmed and booking marked as paid",
+            status=200
+        )
+
+    def handle_payment_intent_payment_failed(self, event):
+        """Handle failed payment"""
+        return HttpResponse(
+            content=f"Payment failed event: {event['type']}",
+            status=200
+        )
