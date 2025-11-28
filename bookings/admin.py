5388413d27ff5278a19e8c3ce1a842cf335daa18
EllisBale <elscar29@gmail.com>
2025-11-28 20:14:50 +0000

    Change total price in bookings admin.py to readonly

--- bookings/admin.py
+++ bookings/admin.py
@@ -7,3 +7,4 @@ class BookingAdmin(admin.ModelAdmin):
     list_display = ("property", "user", "check_in", "check_out", "guests", "total_price", "is_paid")
     list_filter = ("is_paid", "check_in", "check_out")
     search_fields = ("property__property_name", "user__username")
+    readonly_fields = ("total_price",)
