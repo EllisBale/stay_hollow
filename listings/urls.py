96095d0e645e85445fbc689b792dc5e676d5b184
EllisBale <elscar29@gmail.com>
2025-12-06 19:00:31 +0000

    Add search query to listings page

--- listings/urls.py
+++ listings/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.property_list, name='property_list'),
+    path('', views.property_list, name='properties'),
     path('<int:pk>/', views.property_detail, name='property_detail'),
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Property
 
 
@@ -8,7 +10,31 @@ def property_list(request):
     """
 
     properties = Property.objects.all()
-    return render(request, "listings/property_list.html", {"properties": properties})
+    query = None
+
+    if request.GET:
+        if "q" in request.GET:
+            query = request.GET["q"]
+            if not query:
+                messages.error(request, "You didn't enter any search criterial")
+                return redirect(reverse('properties'))
+            
+            queries = (
+                Q(property_name__icontains=query) | 
+                Q(description__icontains=query) |
+                Q(destinations__name__icontains=query) |
+                Q(destinations__parent_destination__name__icontains=query)
+            )
+
+            properties = properties.filter(queries)
+            
+
+    context = {
+        "properties": properties,
+        "search_term": query,
+    }
+
+    return render(request, "listings/property_list.html", context)
 
 
 def property_detail(request, pk):
