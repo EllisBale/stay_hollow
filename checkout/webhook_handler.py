e0db1638ed064c1e0a273a2638c505e964b21ef5
EllisBale <elscar29@gmail.com>
2025-12-10 19:28:03 +0000

    Add confirmation email to webhook_handler.py

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+
 from bookings.models import Booking
 
-import json
 import time
 
 
@@ -10,6 +13,27 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+
+    def _send_confirmation_email(self, order):
+        """
+        Send the user confirmation email
+        """
+        cust_email = order.email
+        subject = render_to_string(
+            "checkout/confirmation_emails/confirmation_email_subject.txt",
+            {"order": order,})
+        body = render_to_string(
+            "checkout/confirmation_emails/confirmation_email_body.txt",
+            {"order": order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )
+        
 
     def handle_event(self, event):
         """
