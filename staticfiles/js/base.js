f76b37a0e5851390d5e9e1af96969ed0c299dd5f
EllisBale <elscar29@gmail.com>
2025-12-14 17:42:23 +0000

    Add back to top btn for desktop and mobile

---
+++ staticfiles/js/base.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+    $("#btn-to-top").on("click", function() {
+        window.scrollTo({ top: 0, behavior: "smooth"})
+    })
+
+
+
+
+
+    const scrollDistance = 500;
+    const desktopWidth = 992;
+
+    function backToTop() {
+        const $btn = $(".back-to-top");
+
+        if (window.innerWidth >= desktopWidth) {
+            $btn
+                .addClass("is-desktop")
+                .removeClass("is-hidden")
+                .show();
+            return;
+        }
+
+        $btn.removeClass("is-desktop");
+
+        if ($(window).scrollTop() > scrollDistance) {
+            $btn.removeClass("is-hidden");
+
+        }
+
+        else {
+            $btn.addClass("is-hidden");
+        }
+    }
+
+    $(window).on("scroll resize", backToTop);
+    backToTop();
+
+
+
+
+
+
+});
