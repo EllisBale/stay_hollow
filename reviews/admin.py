ce02ebff1157857311d4599c6b8ec1bd94338c9f
EllisBale <elscar29@gmail.com>
2025-12-16 19:07:15 +0000

    Add user first name, user email to list display and search_fields

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import Review
-
+from user_account.forms import CustomSignupForm
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = (
         "user",
+        "user__first_name",
+        "user__email",
         "booking",
         "property_name",
         "rating", 
@@ -19,7 +21,9 @@ class ReviewAdmin(admin.ModelAdmin):
     search_fields = (
         "booking__property__property_name",
         "user__username",
-        "comment",                 
+        "comment",
+        "user__first_name",
+        "user__email",
     )
     readonly_fields = (
         "created_at",
