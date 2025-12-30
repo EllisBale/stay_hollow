409245955bac6c9aed87aa231834184b204ac05f
EllisBale <elscar29@gmail.com>
2025-12-30 12:15:19 +0000

    Add about us page

--- home/views.py
+++ home/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from listings.models import Property
 
+
 def index(request):
     """ Returns the index page """
 
     featured_properties = Property.objects.filter(is_featured=True)
 
-    return render(request, 'home/index.html', {"featured_properties": featured_properties})
+    return render(request, "home/index.html", {"featured_properties": featured_properties})
+
+
+def about(request):
+    """ Returns the about page"""
+
+    return render(request, "home/about.html",)
