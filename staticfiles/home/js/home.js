1ec9a5f827fd02d70461be6c758878b52f4f9c05
EllisBale <elscar29@gmail.com>
2026-01-03 07:18:23 +0000

    Add zoom-out scroll animation on searchbar

--- staticfiles/home/js/home.js
+++ staticfiles/home/js/home.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
     const heroImage = document.getElementById("hero-scroll"); // Hero image 
+    const searchBar = document.getElementById("search-transform"); // Searchbar
 
     window.addEventListener("scroll", () => {
         const scrollY = window.scrollY;
         const scale = 1 + scrollY / 1100;
+        const searchScale = 1 - scrollY / 1800;
+        
 
         heroImage.style.transform = `scale(${scale})`;
+        searchBar.style.transform = `
+           translate(-50%, -50%) scale(${searchScale})
+        `;
+        
     });
 
 
