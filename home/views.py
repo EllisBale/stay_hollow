f428b96a8354ebe51d815d1302eedb839f7dc3b8
EllisBale <elscar29@gmail.com>
2026-01-06 18:11:52 +0000

    Add links to popular destination grid

--- home/views.py
+++ home/views.py
@@ -4,7 +4,7 @@ from listings.models import Property
 
 def index(request):
     """ Returns the index page """
-
+    
     featured_properties = Property.objects.filter(is_featured=True)
 
     return render(request, "home/index.html", {"featured_properties": featured_properties})
