7f5ffb0fdfa5ccb06e06593e72ff82ae1dbf2b22
EllisBale <elscar29@gmail.com>
2026-01-10 02:01:55 +0000

    Add clear search button to searchbar

--- listings/static/listings/js/listings.js
+++ listings/static/listings/js/listings.js
@@ -2,14 +2,30 @@
 document.addEventListener("DOMContentLoaded", () => {
     const heroImage = document.getElementById("hero-scroll"); // Hero image 
 
-    if (!heroImage) 
-        return;
-
     window.addEventListener("scroll", () => {
-        const scrollY = window.scrollY;
-        const scale = 1 + scrollY / 1100;
 
-        heroImage.style.transform = `scale(${scale})`;
+        const scrollY = window.scrollY;
+
+        let scale = 1 + scrollY / 1100;
+
+        if (!heroImage) 
+            return;
+
+        if (scale >= 1.8) {
+            heroImage.style.transform = "scale(1.8)";
+        }
+        else {
+            heroImage.style.transform = `scale(${scale})`;
+        }
+    });
+
+    const searchBtn = document.getElementById("clear-btn"); // Clear Button
+    const searchBarText = document.getElementById("search-bar"); // Searchbar
+
+    if (!searchBtn || !searchBarText) return;
+    
+    searchBtn.addEventListener("click", () => {
+        searchBarText.value = "";
     });
 
 });
--- staticfiles/home/js/home.js
+++ staticfiles/home/js/home.js
@@ -1,7 +1,7 @@
 /* jshint esversion: 11 */
 document.addEventListener("DOMContentLoaded", () => {
     const heroImage = document.getElementById("hero-scroll"); // Hero image 
-    const searchBar = document.getElementById("search-transform"); // Searchbar
+    const searchBarTransform = document.getElementById("search-transform"); // Searchbar duv
 
     window.addEventListener("scroll", () => {
         const scrollY = window.scrollY;
@@ -9,19 +9,33 @@ document.addEventListener("DOMContentLoaded", () => {
         if (!heroImage) 
             return;
 
+        if (!searchBarTransform)
+            return;
+
         let scale = 1 + scrollY / 1100;
         let searchScale = 1 - scrollY / 1800;
 
         if (scale >= 1.8) {
-            scale = 1.8;
+            heroImage.style.transform = "scale(1.8)";
         }
         else {
             heroImage.style.transform = `scale(${scale})`;
-            searchBar.style.transform = `
+            searchBarTransform.style.transform = `
                 translate(-50%, -50%) scale(${searchScale})
             `;
         }
         
     });
+
+    const searchBtn = document.getElementById("clear-btn"); // Clear Button
+    const searchBarText = document.getElementById("search-bar"); // Searchbar
+
+    if (!searchBtn || !searchBarText) return;
+
+    searchBtn.addEventListener("click", () => {
+        searchBarText.value = "";
+    });
+
+
 
 });
