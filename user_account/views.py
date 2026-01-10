7e25887133fd583f4ce70fce897348907328c15e
EllisBale <elscar29@gmail.com>
2026-01-10 19:12:09 +0000

    Add password reset and email change to navbar

--- user_account/views.py
+++ user_account/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render, get_object_or_404
-from django.contrib import messages
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 from checkout.models import Order
